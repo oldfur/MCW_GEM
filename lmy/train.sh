3db46f18de87179493423b118d333060932b69de
oldfur <2539550601@qq.com>
2026-01-19 10:09:33 +0800

    nohup ./lmy/train.sh > training_log.txt 2>&1 &

--- lmy/train.sh
+++ lmy/train.sh
@@ -4,4 +4,4 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 torchrun --nproc_per_node=4 --master_port=29601 ./lmy/Train_dist.py
 #python Train.py
 
-# 运行命令：nohup ./run_train.sh > training_log.txt 2>&1 &
+# 运行命令：nohup ./lmy/train.sh > training_log.txt 2>&1 &
